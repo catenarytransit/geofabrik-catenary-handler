d5eda99fb13fc6bdd2af1e31412b9a7e1bee18a6
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2024-07-31 17:58:57 -0700

    Basic downloader and make osm reader

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+use std::io::Write;
+use std::fs;
+
 pub const SOURCES_TO_DOWNLOAD: [&str;22] = [
     "europe/france/alsace",
     "europe/france/aquitaine",
@@ -23,3 +26,35 @@ pub const SOURCES_TO_DOWNLOAD: [&str;22] = [
     "europe/france/rhone-alpes"
 ];
 
+pub async fn download_and_filter_all(temp_pbf_path: &str, poly_folder_path: &str, filtered_folder_path: &str) {
+
+    fs::create_dir_all(temp_pbf_path).unwrap();
+    fs::create_dir_all(poly_folder_path).unwrap();
+    fs::create_dir_all(filtered_folder_path).unwrap();
+
+    let client = reqwest::Client::new();
+
+    for source in SOURCES_TO_DOWNLOAD {
+        let request_url = format!("https://download.geofabrik.de/europe/france/{source}-latest.osm.pbf");
+        let poly_url = format!("https://download.geofabrik.de/europe/{source}.poly");
+
+        let request_pbf = client.get(&request_url).build().unwrap();
+        println!("Downloading {} pbf", source);
+
+        let response_pbf = client.execute(request_pbf).await.unwrap();
+
+        let bytes_from_response_pbf = response_pbf.bytes().await.unwrap();
+
+        println!("Downloaded {} pbf, {:.2} MB", source, bytes_from_response_pbf.as_ref().len() / 1_000_000);
+
+        let read_elements = osmpbf::reader::ElementReader::new(bytes_from_response_pbf.as_ref());
+
+        let mut new_elements: Vec<osmpbf::Element> = vec![];
+
+        let filtered_elements = read_elements.for_each(|element| {
+            let drop =
+
+        });
+
+    }
+}
